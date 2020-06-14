4426ded34da9ea3abb37edaaa2125deb0515189e
Norio <umanari145@gmail.com>
2020-06-14 12:03:33 +0900

    基本のAPI

--- README.md
+++ README.md
@@ -1,9 +1,60 @@
 # firebase
 
+node.jsからfirebaseへの簡易登録API
+
 ## インストール
 
 
 プロジェクト作成機能があるためグローバルでインストール
 ```
 npm install -g firebase-tools
+```
+
+## API一覧
+
+ドメインをhttp://virtual.localと設定
+
+### 新規登録
+
+```
+curl -X POST \
+ -H "Content-Type: application/json" \
+ -d '{"name":"yamada", "email":"test"}' \
+ virtual.local:3000/members
+```
+
+
+### 一覧取得
+
+```
+curl -X GET virtual.local:3000/members | jq
+
+```
+jqでみやすい形に。
+
+
+### 編集
+
+```
+curl -X PUT \
+ -H "Content-Type: application/json" \
+ -d '{"name":"yamada", "email":"test"}' \
+ virtual.local:3000/members/ハッシュキーの値
+
+```
+
+
+
+### 削除
+
+```
+curl -X DELETE \
+virtual.local:3000/members/ハッシュキーの値
+```
+
+### 全削除
+
+```
+curl -X DELETE \
+ virtual.local:3000/members
 ```
--- index.js
+++ index.js
@@ -2,60 +2,81 @@
 const firebase = require('firebase');
 const config = require('./config.js');
 const Sugar = require('sugar');
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express()
+const bodyParser = require('body-parser');
 
 firebase.initializeApp(config.firebaseConfig);
 database = firebase.database();
 
 
 app.use(cors())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-app.get('/create', (req, res) => {
-  let memberRef = database.ref('member');
+//新規追加
+app.post('/:jname', (req, res) => {
 
-  let data = {
-      'email':'hisoka@gmail.com',
-      'name':'hisoka'
-  };
+  let jname = req.params.jname;
+  let Ref = database.ref(jname);
+  let data = req.body;
+  Ref.push(data);
+
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(201).json({result: 'ok',res:data});
 });
 
-app.get('/list', (req, res) => {
-  database.ref('member').once('value', (snapshot) => {
+//一覧取得
+app.get('/:jname', (req, res) => {
+
+  let jname = req.params.jname;
+  database.ref(jname).once('value', (snapshot) => {
+    let items = snapshot.val();
+    let data = [];
+
+    for(var key in items) {
+        let each_item = items[key];
+        each_item['key'] = key;
+        data.push(each_item);
+    }
+
     res.header('Content-Type', 'application/json; charset=utf-8');
-    res.send({member: data});
+
+    let res_data = {};
+    res_data[jname] = data;
+    res.send(res_data);
   });
 });
 
 //個別編集
-app.get('/edit/:hash_key', (req, res) => {
-  let hash_key = req.params.hash_key;
-  //もし存在がなければinsert
-  let data = {
-      'name':'hogehogeaaaaa minamin',
-      'email':'minesoh@gmail.com egashira'
-  };
+app.put('/:jname/:hash_key', (req, res) => {
 
-  database.ref('member').child(hash_key).update(data)
+  let jname = req.params.jname;
+  let hash_key = req.params.hash_key;
+
+  let data = req.body;
+  database.ref(jname).child(hash_key).update(data)
 
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(201).json({result: 'ok',res:data});
 });
 
 //個別削除
-app.get('/delete/:hash_key', (req, res) => {
+app.delete('/:jname/:hash_key', (req, res) => {
+  let jname = req.params.jname;
   let hash_key = req.params.hash_key;
-  database.ref('member').child(hash_key).remove();
+  database.ref(jname).child(hash_key).remove();
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(201).json({result: 'delete ', hash_key: hash_key});
 });
 
 //全削除
-app.get('/deleteAll', (req, res) => {
-  database.ref('member').remove();
+app.delete('/:jname', (req, res) => {
+  let jname = req.params.jname;
+  database.ref(jname).remove();
   res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(201).json({result: 'ok all delete'});
 });
