45e7832410b077ec941c20adab1b9e6aa32d0f48
Norio <umanari145@gmail.com>
2020-06-15 15:10:08 +0900

    vueの基礎ファイル構成を追加

---
+++ src/api/setting.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+export default class settings {
+
+    constructor() {
+
+    }
+
+}
---
+++ src/components/Main.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="wrapper centering">
+      <h1 class="title">
+          ツールリスト
+      </h1>
+      <div id="front" class="centering inner_centering" style="width:80%;" v-cloak>
+          <div>
+              <table class="form">
+                  <tr>
+                      <th>メールアドレス</th>
+                      <td><input type="text" required v-model="settings.email"/></td>
+                  </tr>
+                  <tr>
+                      <th>パスワード</th>
+                      <td><input type="password" required v-model="settings.password"></td>
+                  </tr>
+              </table>
+              <div style="text-align:center;">
+                  <button @click="save_settings">設定保存</button>
+              </div>              
+          </div>
+      </div>
+    <Conditions/>
+  </div>
+</template>
+<script>
+
+import Conditions from './Conditions.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    Conditions
+  },
+  data() {
+     return {
+      settings: [],
+      product_conditions:[],
+      filter_product_conditions:[],
+      delete_ids:[],
+      records: [],
+      is_show_list:0,
+      is_show_edit:0,
+      single_product:{},
+      product_err:'',
+      price_err:'',
+      master_lists: []
+    }
+  },
+  methods:{
+    save_settings() {
+        console.log('aaaa');
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.centering{
+    margin-right: auto;
+    margin-left: auto;
+}
+
+.inner_centering{
+    display: flex;
+    justify-content: center;
+}
+
+.wrapper{
+    width: 80%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
